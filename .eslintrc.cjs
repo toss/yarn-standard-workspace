b6f47a25439cc142eab66f168439b667511aa747
changyoung <changyoung@toss.im>
2023-01-31 21:46:35 +0900

    chore: update eslint settings

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,8 +3,9 @@ module.exports = {
   env: { es6: true, node: true },
   parser: '@typescript-eslint/parser',
   parserOptions: {
+    allowAutomaticSingleRunInference: true,
     tsconfigRootDir: __dirname,
-    project: ['./packages/*/tsconfig.json'],
+    project: ['./tsconfig.eslint.json', './packages/*/tsconfig.json'],
   },
   plugins: ['@typescript-eslint'],
   extends: [
@@ -12,5 +13,13 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'prettier',
+  ],
+  overrides: [
+    {
+      files: ['.eslintrc.cjs'],
+      rules: {
+        '@typescript-eslint/no-unsafe-assignment': 'off',
+      },
+    },
   ],
 };
